f6db7485a8a94c4512e396e802571175f5318553
Andrés Ingelmo Poveda <andrw333@gmail.com>
2022-07-16 00:36:57 +0200

    Add login and user page

--- wherever-it-takes/app.py
+++ wherever-it-takes/app.py
@@ -1,11 +1,28 @@
-from flask import Flask, render_template
+from flask import Flask, redirect, render_template, request, session, url_for
 
 app = Flask(__name__)
+app.secret_key = "hello"
 
 
 @app.route("/")
 def home():
     return render_template("index.html")
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        username = request.form["username"]
+        session["username"] = username
+        return redirect(url_for("user"))
+
+    else:
+        return render_template("login.html")
+
+
+@app.route("/user")
+def user():
+    return render_template("user.html")
 
 
 if __name__ == "__main__":

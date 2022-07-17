624c4b764c39770fef7d0eeab28d5eac23879b8d
Andrés Ingelmo Poveda <andrw333@gmail.com>
2022-07-17 11:18:09 +0200

    Add basic structure for auth and view files

--- full-tutorial-twt/website/__init__.py
+++ full-tutorial-twt/website/__init__.py
@@ -5,4 +5,10 @@ def create_app():
     app = Flask(__name__)
     app.config["SECRET_KEY"] = "dev"
 
+    from .auth import auth
+    from .views import views
+
+    app.register_blueprint(views, url_prefix="/")
+    app.register_blueprint(auth, url_prefix="/")
+
     return app

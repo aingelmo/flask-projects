f7115b3d268ec9ebdfbd026bf27c2d4b19248e18
Andrés Ingelmo Poveda <andrw333@gmail.com>
2022-07-17 16:55:36 +0200

    Add database creation code

--- full-tutorial-twt/website/__init__.py
+++ full-tutorial-twt/website/__init__.py
@@ -1,9 +1,17 @@
+from os import path
+
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+DB_NAME = "database.db"
 
 
 def create_app():
     app = Flask(__name__)
     app.config["SECRET_KEY"] = "dev"
+    app.config["SQLALCHEMY_DATABASE_URI"] = f"sqlite:///{DB_NAME}"
+    db.init_app(app)
 
     from .auth import auth
     from .views import views
@@ -11,4 +19,14 @@ def create_app():
     app.register_blueprint(views, url_prefix="/")
     app.register_blueprint(auth, url_prefix="/")
 
+    from .models import Note, User
+
+    create_database(app)
+
     return app
+
+
+def create_database(app):
+    if not path.exists("website" + DB_NAME):
+        db.create_all(app=app)
+        print("Created Database!")

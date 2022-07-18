eebfc41bb973340225cae7d44d4627e27697980d
Andrés Ingelmo Poveda <andrw333@gmail.com>
2022-07-19 00:01:17 +0200

    Refactor application

--- wherever-it-takes/website/models.py
+++
@@ -1,9 +0,0 @@
-from flask_login import UserMixin
-
-from . import db
-
-
-class Users(db.Model, UserMixin):
-    id = db.Column("student_id", db.Integer, primary_key=True)
-    email = db.Column("email", db.String(50))
-    password = db.Column("password", db.String(50))

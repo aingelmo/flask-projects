fcf56650336d244a2b0476a9fbe6285fba36dfb2
Andrés Ingelmo Poveda <andrw333@gmail.com>
2022-07-19 19:46:27 +0200

    Add database creation

--- wherever-it-takes/website/__init__.py
+++ wherever-it-takes/website/__init__.py
@@ -12,3 +12,17 @@ def create_app():
     app.config.from_mapping(
         SECRET_KEY="dev", SQLALCHEMY_DATABASE_URI=f"sqlite:///{DB_NAME}"
     )
+
+    from .auth import bp
+
+    app.register_blueprint(bp)
+
+    from .models import Users
+
+    db.init_app(app)
+
+    @app.route("/hello")
+    def hello():
+        return "Hello World"
+
+    return app

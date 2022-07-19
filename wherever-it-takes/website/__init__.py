7bfdc1cd9149566bbdc143d1a39d228e69929817
Andrés Ingelmo Poveda <andrw333@gmail.com>
2022-07-19 23:24:23 +0200

    Add login manager

--- wherever-it-takes/website/__init__.py
+++ wherever-it-takes/website/__init__.py
@@ -1,6 +1,7 @@
 from os import path
 
 from flask import Flask
+from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
@@ -10,7 +11,9 @@ DB_NAME = "database.db"
 def create_app():
     app = Flask(__name__)
     app.config.from_mapping(
-        SECRET_KEY="dev", SQLALCHEMY_DATABASE_URI=f"sqlite:///{DB_NAME}"
+        SECRET_KEY="dev",
+        SQLALCHEMY_DATABASE_URI=f"sqlite:///{DB_NAME}",
+        SQLALCHEMY_TRACK_MODIFICATIONS=False,
     )
     db.init_app(app)
 
@@ -21,6 +24,14 @@ def create_app():
     from .models import Users
 
     create_database(app)
+
+    login_manager = LoginManager()
+    login_manager.login_view = "auth.login"
+    login_manager.init_app(app)
+
+    @login_manager.user_loader
+    def load_user(id):
+        return Users.query.get(int(id))
 
     @app.route("/hello")
     def hello():
--- wherever-it-takes/website/models.py
+++ wherever-it-takes/website/models.py
@@ -3,8 +3,8 @@ from flask_login import UserMixin
 from . import db
 
 
-class Users(db.Model):
+class Users(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
-    user = db.Column(db.String(50))
+    username = db.Column(db.String(50))
     email = db.Column(db.String(100))
     password = db.Column(db.String(100))
